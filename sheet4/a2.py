e387df2e0e5f27390c84b8b7ad810810ad610d5b
Christopher Hasenberg <christopher.hasenberg@hotmail.de>
2015-11-29 21:57:41 +0100

    progress

--- sheet4/a2.py
+++ sheet4/a2.py
@@ -5,8 +5,28 @@ import math as m
 
 datei = r.TFile("/Users/christopher/Dropbox/SMD/Code/Blatt4/populationen.root", "RECREATE")
 
-data = np.zeros((3,1))
+data = np.zeros((2,1))
 tree_1 = r.TTree("population1","population1")
 tree_1.Branch("x", data[0],"x/D")
+n = 10000
+x_min = -15
+histogramm = r.TH1D("histogramm", "histogramm",40,x_min,15)
+def population1(n):
+	mu_x = 0
+	sigma_x = 4
+	mu_y = 1.5
+	sigma_y = 1.5
+	rnd1 = r.TRandom3(1);
+	
+	for i in range(n):
+            data[0] = np.random.normal(mu_x,sigma_x)
+            tree_1.Fill()
+            histogramm.Fill(data[0])
+	return histogramm
 
-def 2populationen()
+population = r.TCanvas("population", "population", 800, 600)
+population.SetLogy()
+signal = population1(n)
+signal.DrawCopy()
+population.SaveAs("/Users/christopher/Dropbox/SMD/Code/Blatt4/a2.pdf")
+
