57dbe843c7e9c77fdede09b5353adb85ee672645
Christopher Hasenberg <christopher.hasenberg@hotmail.de>
2015-12-01 00:08:32 +0100

    enhanced

--- sheet4/a2.py
+++ sheet4/a2.py
@@ -3,6 +3,7 @@ import ROOT as r
 import numpy as np
 import math as m
 
+########### Aufgabe 2a) ##############
 datei = r.TFile("/Users/christopher/Dropbox/SMD/Code/Blatt4/populationen.root", "RECREATE")
 
 data = np.zeros((4,1))
@@ -11,6 +12,13 @@ tree_2 = r.TTree("population2","population2")
 tree_1.Branch("x", data[0],"x/D")
 tree_2.Branch("y", data[1],"x/D")
 n = 10000
+mu_x1 = 0
+sigma_x1 = 1.5
+mu_y1 = 4
+sigma_y1 = 1.5
+
+histogramm = r.TH2D("histogramm", "histogramm",30,15,15,30,-10,15)
+histogramm2 = r.TH2D("histogramm2", "histogramm2",30,15,15,30,-10,15)
 
 histogramm = r.TH2D("histogramm", "histogramm",30,15,15,30,-10,15)
 histogramm2 = r.TH2D("histogramm2", "histogramm2",30,15,15,30,-10,15)
@@ -19,19 +27,13 @@ def population1(n):
 	sigma_x1 = 1.5
 	mu_y1 = 4
 	sigma_y1 = 1.5
-	mu_x2 = 4
-	sigma_x2 = 3.5
-	sigma_y2 = 0.9
+
 	for i in range(n):
             data[0] = np.random.normal(mu_x1,sigma_x1)
             data[1] = np.random.normal(mu_y1,sigma_y1)
             tree_1.Fill()
             histogramm.Fill(data[0],data[1])
-            data[2] = np.random.normal(mu_x2,sigma_x2)
-            mu_y2 = 2+0.35*(data[2]-4)
-            data[3] = np.random.normal(mu_y2,sigma_y2)
-            tree_2.Fill()
-            histogramm2.Fill(data[2],data[3])
+	print(len(data[0]))
 	return histogramm
 
 def population2(n):
@@ -44,6 +46,7 @@ def population2(n):
             data[3] = np.random.normal(mu_y2,sigma_y2)
             tree_2.Fill()
             histogramm2.Fill(data[2],data[3])
+	
 	return histogramm2
 
 population = r.TCanvas("population", "population", 800, 600)
@@ -51,6 +54,75 @@ population = r.TCanvas("population", "population", 800, 600)
 pop1 = population1(n)
 pop1.DrawCopy("COLZ")
 pop2 = population2(n)
-pop2.Draw("sames")
-population.SaveAs("/Users/christopher/Dropbox/SMD/Code/Blatt4/a2.pdf")
+
+############ Aufgabe 2b) #################
+
+#Population 1 
+for i in range(n):
+	data[0] = np.random.normal(loc=mu_x1,scale=sigma_x1)
+	data[1] = np.random.normal(loc=mu_y1,scale=sigma_y1)
+	tree_1.Fill()
+	histogramm.Fill(data[0],data[1])
+
+# Berechnung der Mittelwerte 
+x1_sum= np.sum(data[0])
+length1 = len(data[0])
+x1_mean = x1_sum/n
+y1_sum= np.sum(data[1])
+length2 = len(data[1])
+y1_mean = y1_sum/n
+print("Mean X_1:",x1_mean)
+print("Mean Y_1:",y1_mean)
+
+#Berechnung der Kovarianzmatrix
+cov_0 = np.array([np.zeros(2),np.zeros(2)])
+cov_0[0][0] = np.var(data[0], ddof = 0)
+cov_0[1][0] = (np.dot((data[0]-x1_mean),(data[1]-y1_mean)))/(n-1)
+cov_0[0][1] = cov_0[1][0]
+cov_0[1][1] = np.var(data[1], ddof = 0)
+korrel_0 = cov_0[1][0]/(m.sqrt(cov_0[1][1])*m.sqrt(cov_0[0][0]))
+print("Kovarianzmatrix:","\n")
+print(cov_0[0][0],"\t",cov_0[1][0],"\n")
+print(cov_0[0][1],"\t",cov_0[1][1],"\n")
+print("Korrelationskoeffizient: ",korrel_0,"\n")
+
+# Population 2
+mu_x2 = 4
+sigma_x2 = 3.5
+sigma_y2 = 0.9
+for i in range(n):
+    data[2] = np.random.normal(loc=mu_x2,scale=sigma_x2)
+    mu_y2 = 2+0.35*(data[2]-4)
+    data[3] = np.random.normal(loc=mu_y2,scale=sigma_y2)
+    tree_2.Fill()
+    histogramm2.Fill(data[2],data[3])
+
+#Berechnung der Mittelwerte
+x2_sum= np.sum(data[2])
+length3 = len(data[2])
+x2_mean = x2_sum/n
+y2_sum= np.sum(data[3])
+length4 = len(data[3])
+y2_mean = y2_sum/n
+print("Mean X_2:",x2_mean)
+print("Mean Y_2:",y2_mean)
+
+
+#Berechnung der Kovarianzmatrix
+cov_2 = np.array([np.zeros(2),np.zeros(2)])
+cov_2[0][0] = np.var(data[2], ddof = 0)
+cov_2[1][0] = (np.dot((data[2]-x2_mean),(data[3]-y2_mean)))/(n-1)
+cov_2[0][1] = cov_2[1][0]
+cov_2[1][1] = np.var(data[3], ddof = 0)
+korrel_2 = cov_2[1][0]/(m.sqrt(cov_2[1][1])*m.sqrt(cov_2[0][0]))
+print("Kovarianzmatrix:","\n")
+print(cov_2[0][0],"\t",cov_2[1][0],"\n")
+print(cov_2[0][1],"\t",cov_2[1][1],"\n")
+print("Korrelationskoeffizient: ",korrel_0,"\n")
+
+
+# Berechnung der statistsischen Groessen fuer die Gesamtheit
+
+
+
 
