f56d9eeba19c8bcfcd3b647d5541f8e158f4ea87
Christopher Hasenberg <christopher.hasenberg@hotmail.de>
2015-12-01 18:47:14 +0100

    works

--- sheet4/a2.py
+++ sheet4/a2.py
@@ -1,149 +1,123 @@
 from __future__ import print_function, unicode_literals, division
-import ROOT as r
+
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
-import math as m
+import ROOT as r
 
-########### Aufgabe 2a) ##############
-datei = r.TFile("/Users/christopher/Dropbox/SMD/Code/Blatt4/populationen.root", "RECREATE")
+#Anlegen des ROOT-Files
+datei = r.TFile("/Users/christopher/Dropbox/SMD/Code/Blatt4/a2.root","RECREATE")
 
-data = np.zeros((4,1))
-tree_1 = r.TTree("population1","population1")
-tree_2 = r.TTree("population2","population2")
-tree_1.Branch("x", data[0],"x/D")
-tree_2.Branch("y", data[1],"x/D")
-n = 10000
-mu_x1 = 0
-sigma_x1 = 1.5
-mu_y1 = 4
-sigma_y1 = 1.5
+# Aufgabenteil a)
+#Populationszahl
+n = 10000 
+#Erzeugung der Zufallszahlen mit den gegebenen Informationen
+x1 = np.random.normal(0, 1.5, n)
+y1 = np.random.normal(4, 1.5, n)
+x2 = np.random.normal(4, 3.5, n)
+y2 = np.zeros(n)
 
-histogramm = r.TH2D("histogramm", "histogramm",30,15,15,30,-10,15)
-histogramm2 = r.TH2D("histogramm2", "histogramm2",30,15,15,30,-10,15)
+for i, x in zip(range(len(x2)),x2):
+    y2[i] = np.random.normal(2 + 0.35 * (x - 4), 0.9)
+#Plotten der Daten in 2D-Histogramme fuer die erste Population
+fig = plt.figure()    
+plt.hist2d(x1, y1, bins=30, norm=LogNorm(), alpha=1)
+plt.colorbar()
+fig = plt.figure()
+plt.hist2d(x2, y2, bins=30, norm=LogNorm(), alpha=1)
+plt.colorbar()
+fig = plt.figure()    
+plt.hist2d(x1, y1, bins=30, norm=LogNorm(), alpha=1)
+plt.colorbar()
+plt.hist2d(x2, y2, bins=30, norm=LogNorm(), alpha=0.7)
+plt.ylim([-4,10])
+plt.colorbar()
+plt.show()
 
-histogramm = r.TH2D("histogramm", "histogramm",30,15,15,30,-10,15)
-histogramm2 = r.TH2D("histogramm2", "histogramm2",30,15,15,30,-10,15)
+#Aufgabenteil b)
 
-cov_12 = np.array([np.zeros(2),np.zeros(2)])
+#Hier werden nun die Mittelwerte,Varianzen und die Kovarianzen der einzelnen Verteilungen berechnet
+x1_mean = np.mean(x1)
+y1_mean = np.mean(y1)
+x2_mean = np.mean(x2)
+y2_mean = np.mean(y2)
+#Varianz
+x1_var = np.var(x1)
+y1_var = np.var(y1)
+x2_var = np.var(x2)
+y2_var = np.var(y2)
+#Kovarianz der 1. Population
+x1_covariance = np.cov(x1)
+y1_covariance = np.cov(y1)
+#Kovarianz der 2. Population
+x2_covariance = np.cov(x2)
+y2_covariance = np.cov(y2)
+#Korrelationkoeffizienten rho
+x1_correlcoeff = np.correlcoeff(x1)
+y1_correlcoeff = np.correlcoeff(y1)
+x2_correlcoeff = np.correlcoeff(x2)
+y2_correlcoeff = np.correlcoeff(y2)
+#Kovarianz der beiden Verteilungen
+covariance1 = np.cov(np.array([x1,y1]))
+covariance2 = np.cov(np.array([x2,y2]))
 
-def population1(n):
-	mu_x1 = 0
-	sigma_x1 = 1.5
-	mu_y1 = 4
-	sigma_y1 = 1.5
+# Berechnung der Korrelationskoeffizienten der Gesamtheiten
+correlcoeff1 = np.correlcoeff(np.array([x1,y1]))
+correlcoeff2 = np.correlcoeff(np.array([x2,y2]))
 
-	for i in range(n):
-            data[0] = np.random.normal(mu_x1,sigma_x1)
-            data[1] = np.random.normal(mu_y1,sigma_y1)
-            tree_1.Fill()
-            histogramm.Fill(data[0],data[1])
-	print(len(data[0]))
-	return histogramm
+#Ausgabe der ermittelte Größen
+print("Mittelwerte")
+print(x1_mean, y1_mean, x2_mean, y2_mean)
+print("\n Varianz")
+print(x1_var, y1_var, x2_var, y2_var)
+print("\n Einzeln Kovarianz")
+print(x1_covariance, y1_covariance, x2_covariance, y2_covariance)
+print("\n Einzeln Korrelation")
+print(x1_correlcoeff, y1_correlcoeff, x2_correlcoeff, y2_correlcoeff)
+print("\n Kovarianz")
+print(covariance1)
+print(covariance2)
+print("\n Korrelation")
+print(correlcoeff1)
+print(correlcoeff2)
 
-def population2(n):
-	mu_x2 = 4
-	sigma_x2 = 3.5
-	sigma_y2 = 0.9
-	for i in range(n):
-            data[2] = np.random.normal(mu_x2,sigma_x2)
-            mu_y2 = 2+0.35*(data[2]-4)
-            data[3] = np.random.normal(mu_y2,sigma_y2)
-            tree_2.Fill()
-            histogramm2.Fill(data[2],data[3])
-	
-	return histogramm2
-
-population = r.TCanvas("population", "population", 800, 600)
-#population.SetLogy()
-pop1 = population1(n)
-pop1.DrawCopy("COLZ")
-pop2 = population2(n)
-
-############ Aufgabe 2b) #################
-
-#Population 1 
+#Aufgabenteil c)
+#Abspeichern in ROOT-File: Population 1
+tree_1 = r.TTree("Population 1", "Population 1")
+data = np.zeros((2,1))
+tree_1.Branch("x1", data[0], "x/D")
+tree_1.Branch("y1", data[1], "x/D")
 for i in range(n):
-	data[0] = np.random.normal(loc=mu_x1,scale=sigma_x1)
-	data[1] = np.random.normal(loc=mu_y1,scale=sigma_y1)
-	tree_1.Fill()
-	histogramm.Fill(data[0],data[1])
+    data[0] = np.random.normal(0, 1.5)
+    tree_1.Fill()
+    data[1] = np.random.normal(4,1.5)
+    tree_1.Fill
+tree_1.Write()
 
-# Berechnung der Mittelwerte 
-x1_sum= np.sum(data[0])
-length1 = len(data[0])
-x1_mean = x1_sum/n
-y1_sum= np.sum(data[1])
-length2 = len(data[1])
-y1_mean = y1_sum/n
-print("Mean X_1:",x1_mean)
-print("Mean Y_1:",y1_mean)
-
-#Berechnung der Kovarianzmatrix
-cov_0 = np.array([np.zeros(2),np.zeros(2)])
-cov_0[0][0] = np.var(data[0], ddof = 0)
-cov_0[1][0] = (np.dot((data[0]-x1_mean),(data[1]-y1_mean)))/(n-1)
-cov_0[0][1] = cov_0[1][0]
-cov_0[1][1] = np.var(data[1], ddof = 0)
-korrel_0 = cov_0[1][0]/(m.sqrt(cov_0[1][1])*m.sqrt(cov_0[0][0]))
-print("Kovarianzmatrix:","\n")
-print(cov_0[0][0],"\t",cov_0[1][0],"\n")
-print(cov_0[0][1],"\t",cov_0[1][1],"\n")
-print("Korrelationskoeffizient: ",korrel_0,"\n")
-
-# Population 2
-mu_x2 = 4
-sigma_x2 = 3.5
-sigma_y2 = 0.9
+#Abspeichern in ROOT-File: Population 2
+tree_2 = r.TTree("Population 2", "Population 2")
+data2 = np.zeros((2,1))
+tree_2.Branch("x2", data2[0], "x/D")
+tree_2.Branch("y2", data2[1], "x/D")
 for i in range(n):
-    data[2] = np.random.normal(loc=mu_x2,scale=sigma_x2)
-    mu_y2 = 2+0.35*(data[2]-4)
-    data[3] = np.random.normal(loc=mu_y2,scale=sigma_y2)
+    data2[0] = np.random.normal(0, 1.5)
     tree_2.Fill()
-    histogramm2.Fill(data[2],data[3])
+    mu_y = 2 + 0.35 * (data2[0] - 4)
+    data2[1] = np.random.normal(mu_y, 0.9)
+    tree_2.Fill
+tree_2.Write()
 
-#Berechnung der Mittelwerte
-x2_sum= np.sum(data[2])
-length3 = len(data[2])
-x2_mean = x2_sum/n
-y2_sum= np.sum(data[3])
-length4 = len(data[3])
-y2_mean = y2_sum/n
-print("Mean X_2:",x2_mean)
-print("Mean Y_2:",y2_mean)
-
-
-#Berechnung der Kovarianzmatrix
-cov_2 = np.array([np.zeros(2),np.zeros(2)])
-cov_2[0][0] = np.var(data[2], ddof = 0)
-cov_2[1][0] = (np.dot((data[2]-x2_mean),(data[3]-y2_mean)))/(n-1)
-cov_2[0][1] = cov_2[1][0]
-cov_2[1][1] = np.var(data[3], ddof = 0)
-korrel_2 = cov_2[1][0]/(m.sqrt(cov_2[1][1])*m.sqrt(cov_2[0][0]))
-print("Kovarianzmatrix:","\n")
-print(cov_2[0][0],"\t",cov_2[1][0],"\n")
-print(cov_2[0][1],"\t",cov_2[1][1],"\n")
-print("Korrelationskoeffizient: ",korrel_0,"\n")
-
-
-#kombinierte Kovarianzmatrix
-cov_12[0][0] = 0.5*(cov_1[0][0]+cov_2[0][0]);
-cov_12[1][0] = 0.5*(cov_1[1][0]+cov_2[1][0]);
-cov_12[0][1] = cov_12[1][0];
-cov_12[1][1] = 0.5*(cov_1[1][1]+cov_2[1][1]);
-#aenderung der kovarianz matrix -> 4x4?
-#alternativ:
-
-cov_121 = np.array([np.zeros(4),np.zeros(4)])
-cov_121[0][0] = cov_1[0][0]
-cov_121[1][0] = (np.dot((data[1]-y1_mean),(data[0]-x1_mean)))/(n-1)
-cov_121[2][0] = (np.dot((data[2]-x2_mean),(data[0]-x1_mean)))/(n-1)
-cov_121[3][0] = (np.dot((data[3]-y2_mean),(data[0]-x1_mean)))/(n-1)
-cov_121[0][1] = cov_121[1][0]
-cov_121[0][1] = cov_121[1][0]
-#Ausgabe
-print("Kombinierte Kovarianzmatrix:","\n")
-print(cov_12[0][0],"\t",cov_12[1][0],"\n")
-print(cov_12[0][1],"\t",cov_12[1][1],"\n")
-
-
-
+#Abspeichern in ROOT-File: Population 3
+tree_3 = r.TTree("Population 3", "Population 3")
+n2 =1000
+data3 = np.zeros((2,1))
+tree_3.Branch("x3", data3[0], "x/D")
+tree_3.Branch("y3", data3[1], "x/D")
+for i in range(n2):
+    data3[0] = np.random.normal(0, 1.5)
+    tree_3.Fill()
+    data3[1] = np.random.normal(4,1.5)
+    tree_3.Fill
+tree_3.Write()
 
