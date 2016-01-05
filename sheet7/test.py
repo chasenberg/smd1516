422a975dc8e6fc549d51c9ca9c899742b77c23f3
Christopher Hasenberg <christopher.hasenberg@hotmail.de>
2016-01-05 22:54:59 +0100

    scikit test

---
+++ sheet7/test.py
@@ -0,0 +1,8 @@
+from sklearn import tree
+import pandas as pd 
+import numpy as np 
+
+gamma = pd.read_csv('/Users/christopher/Downloads/Gamma-MC.csv')
+proton = pd.read_csv('/Users/christopher/Downloads/Proton-MC.csv')
+
+print(gamma)
